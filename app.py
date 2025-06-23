ce7c662309dc6a9dbef4d34df4b6688d9651a3f9
atepginzo <atepginzo037@gmail.com>
2025-06-23 12:41:38 +0700

    tambah requirements.txt dan Procfile untuk Railway

--- app.py
+++ app.py
@@ -54,5 +54,7 @@ def predict():
         error_message = f"Terjadi error: {e}"
         return render_template('index.html', prediction=error_message)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
